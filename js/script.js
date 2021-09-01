b93ef035e4405d7dcf1a80fa654521d3098d7944
Valentina-Fr <v_frasca@virgilio.it>
2021-09-01 18:04:37 +0200

    Fix for loop

--- js/script.js
+++ js/script.js
@@ -9,7 +9,7 @@ const app = new Vue ({
 
     },
     created() {
-        for(i = 0; i < 11; i++){
+        for(let i = 0; i < 10; i++){
             axios
             .get("https://flynn.boolean.careers/exercises/api/random/mail")
             .then((res) => {
